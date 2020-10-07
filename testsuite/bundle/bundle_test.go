73f8e32ff949b8e97b07411cf6944b277ca3b31a
Fabian Martinez <famartin@redhat.com>
2020-10-07 11:33:56 +0200

    allow to install operator from deploy folder

--- testsuite/bundle/bundle_test.go
+++ testsuite/bundle/bundle_test.go
@@ -3,6 +3,7 @@ package bundle
 import (
 	"math/rand"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -26,19 +27,34 @@ func installOperator() {
 
 	kubernetesutils.CreateTestNamespace(suiteCtx.Clientset, utils.OperatorNamespace)
 
+	log.Info("Installing operator")
 	if strings.HasPrefix(utils.OperatorBundlePath, "https://") {
 		bundlePath = "/tmp/apicurio-operator-bundle-" + strconv.Itoa(rand.Intn(1000)) + ".yaml"
 		utils.DownloadFile(bundlePath, utils.OperatorBundlePath)
+
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", bundlePath)
+	} else if strings.HasSuffix(utils.OperatorBundlePath, ".yaml") {
+		file := utils.Template("operator-bundle", bundlePath,
+			utils.Replacement{Old: "{NAMESPACE}", New: utils.OperatorNamespace},
+		)
+		bundlePath = file.Name()
+
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", bundlePath)
+	} else {
+
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "service_account.yaml"))
+		//maybe set pull secret
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "role.yaml"))
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "role_binding.yaml"))
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "cluster_role.yaml"))
+		clusterRoleBindingFile := utils.Template("cluster_role_binding", filepath.Join(bundlePath, "cluster_role_binding.yaml"),
+			utils.Replacement{Old: "{NAMESPACE}", New: utils.OperatorNamespace},
+		)
+		defer os.Remove(clusterRoleBindingFile.Name())
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", clusterRoleBindingFile.Name())
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "crds/apicur.io_apicurioregistries_crd.yaml"))
+		kubernetescli.Execute("apply", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "operator.yaml"))
 	}
-
-	file := utils.Template("operator-bundle", bundlePath,
-		utils.Replacement{Old: "{NAMESPACE}", New: utils.OperatorNamespace},
-	)
-
-	bundlePath = file.Name()
-
-	log.Info("Installing operator")
-	kubernetescli.Execute("apply", "-f", bundlePath, "-n", utils.OperatorNamespace)
 
 	kubernetesutils.WaitForOperatorDeploymentReady(suiteCtx.Clientset)
 
@@ -53,7 +69,18 @@ func uninstallOperator() {
 	utils.SaveOperatorLogs(suiteCtx.Clientset, suiteCtx.SuiteID)
 
 	log.Info("Uninstalling operator")
-	kubernetescli.Execute("delete", "-f", bundlePath, "-n", utils.OperatorNamespace)
+
+	if strings.HasSuffix(bundlePath, ".yaml") {
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", bundlePath)
+	} else {
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "operator.yaml"))
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "crds/apicur.io_apicurioregistries_crd.yaml"))
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "cluster_role_binding.yaml"))
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "cluster_role.yaml"))
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "role_binding.yaml"))
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "role.yaml"))
+		kubernetescli.Execute("delete", "-n", utils.OperatorNamespace, "-f", filepath.Join(bundlePath, "service_account.yaml"))
+	}
 
 	kubernetesutils.WaitForOperatorDeploymentRemoved(suiteCtx.Clientset)
 
