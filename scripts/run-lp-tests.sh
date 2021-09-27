08dfc470860b3653afab8c53603bd3da1186ea49
Fabian Martinez <famartin@redhat.com>
2021-09-27 15:11:02 +0200

    add lp tests script

---
+++ scripts/run-lp-tests.sh
@@ -0,0 +1,4 @@
+E2E_OLM_USE_DEFAULT_CATALOG_SOURCE=redhat-operators \
+ OLM_CATALOG_SOURCE_NAMESPACE=openshift-marketplace \
+ OLM_CLUSTER_WIDE_OPERATORS_NAMESPACE=openshift-operators \
+ OLM_PACKAGE_MANIFEST_NAME=service-registry-operator make run-operator-tests
