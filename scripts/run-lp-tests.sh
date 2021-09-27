8b70fc0c81ca8125555f06c4ed3f2a1555f641fa
Fabian Martinez <famartin@redhat.com>
2021-09-27 16:28:21 +0200

    try fix lp tests script

--- scripts/run-lp-tests.sh
+++ scripts/run-lp-tests.sh
@@ -1,4 +1,8 @@
 E2E_OLM_USE_DEFAULT_CATALOG_SOURCE=redhat-operators \
- OLM_CATALOG_SOURCE_NAMESPACE=openshift-marketplace \
- OLM_CLUSTER_WIDE_OPERATORS_NAMESPACE=openshift-operators \
- OLM_PACKAGE_MANIFEST_NAME=service-registry-operator make run-lp-tests
+    E2E_OLM_CATALOG_SOURCE_NAMESPACE=openshift-marketplace \
+    E2E_OLM_CLUSTER_WIDE_OPERATORS_NAMESPACE=openshift-operators \
+    E2E_OLM_PACKAGE_MANIFEST_NAME=service-registry-operator \
+    E2E_STRIMZI_BUNDLE_PATH=https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.23.0/strimzi-cluster-operator-0.23.0.yaml \
+    go run github.com/onsi/ginkgo/ginkgo -r --randomize-all --randomize-suites --fail-on-pending --keep-going \
+    --junit-report=xunit-report.xml \
+    --cover --trace --race --progress -v ./testsuite/olm -- -only-test-operator -disable-clustered-tests -enable-olm-advanced-tests
