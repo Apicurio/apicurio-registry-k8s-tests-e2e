749ebc25066401dad92fd65ab49852da00eeb6d0
Fabian Martinez <famartin@redhat.com>
2021-11-25 15:39:10 +0100

    update kind to v0.11.1

--- scripts/setup-deps.sh
+++ scripts/setup-deps.sh
@@ -1,7 +1,7 @@
 if ! command -v kind &> /dev/null
 then
     echo "Installing kind"
-    curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
+    curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.11.1/kind-$(uname)-amd64"
     chmod +x ./kind
 fi
 
